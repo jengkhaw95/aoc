3585b0df21cf3dfd0f91be63a69817579f90291d
jeng <jengkhaw@overwatchs.com>
2023-12-07 10:57:01 +0800

    finally

--- 2023/5-2/code.ts
+++ 2023/5-2/code.ts
@@ -1,6 +1,87 @@
 import { readFileSync } from "fs";
 import path from "path";
 
+const useLog = !true;
+function mergeRanges(ranges: number[][]) {
+  if (!Array.isArray(ranges) || ranges.length === 0) {
+    return [];
+  }
+
+  // Sort the ranges based on their start values
+  ranges.sort((a, b) => a[0] - b[0]);
+
+  const mergedRanges = [ranges[0]];
+
+  for (let i = 1; i < ranges.length; i++) {
+    const currentRange = ranges[i];
+    const lastMergedRange = mergedRanges[mergedRanges.length - 1];
+
+    if (currentRange[0] <= lastMergedRange[1]) {
+      // Ranges overlap, merge them
+      lastMergedRange[1] = Math.max(lastMergedRange[1], currentRange[1]);
+    } else {
+      // Ranges do not overlap, add the current range to the merged list
+      mergedRanges.push(currentRange);
+    }
+  }
+
+  return mergedRanges;
+}
+
+function splitArray(origin: number[][], filter: number[][]) {
+  let _origin = origin;
+  let offsetRanges: number[][] = [];
+  let passedRanges: number[][] = [];
+  for (let f of filter) {
+    const [dest, src, count] = f;
+
+    const [fmin, fmax] = [src, src + count - 1];
+
+    useLog && console.log("\n>>   Filter  ", f, "of", filter);
+
+    for (let o of _origin) {
+      const tpr: number[][] = [];
+      useLog && console.log(">>     Process ", o, "of", _origin);
+      const [min, max] = o;
+
+      if (fmin > max || fmax < min) {
+        useLog && console.log(">>       Skipped ", f);
+        continue;
+      }
+      useLog && console.log(">>       Pass    ", o);
+      if (min < fmin) {
+        tpr.push([min, fmin - 1]);
+        passedRanges.push([min, fmin - 1]);
+      }
+      if (max > fmax) {
+        tpr.push([fmax + 1, max]);
+        passedRanges.push([fmax + 1, max]);
+      }
+
+      passedRanges = tpr;
+
+      // filter & offset
+      const [amin, amax] = [Math.max(min, fmin), Math.min(max, fmax)];
+      const offset = dest - fmin;
+      offsetRanges.push([amin + offset, amax + offset]);
+      useLog && console.log(">>       Out     ", passedRanges);
+      useLog &&
+        console.log(">>       Filtered", [amin, amax], " > ", offsetRanges);
+    }
+    if (passedRanges.length || offsetRanges.length) {
+      _origin = passedRanges;
+    }
+    // _origin = out;
+    // console.log(out);
+  }
+
+  // Merge
+  useLog && console.log({ origin, _origin, offsetRanges, filter });
+  const result = mergeRanges([..._origin, ...offsetRanges]);
+  useLog && console.log(result);
+  return result;
+}
+
 async function main() {
   const input = await readFileSync(
     path.resolve(__dirname, "./input.txt"),
@@ -24,253 +105,34 @@ async function main() {
     .map((d) => d.split("\r\n").filter((_, i, f) => i !== f.length - 1))
     .map((d) => d.map((e) => e.split(" ").map(Number)));
 
-  // let __min = Infinity;
-
-  // const __seeds = seeds.reduce(
-  //   (a, b) => [...a, [b[0], b[0] + b[1]]],
-  //   [] as number[][]
-  // );
-
-  // for (let _s of __seeds) {
-  //   for (let s of Array.from(new Array(_s[1])).map((d,i)=>_s[0]+i)) {
-  //     let d = s;
-  //     let _min = Infinity;
-  //     for (let mapType of mapTypes) {
-  //       for (let map of mapType) {
-  //         const [dest, src, count] = map;
-  //         const mapMin = src;
-  //         const mapMax = src + count - 1;
-  //         if (mapMin <= s && s < mapMax) {
-  //           d=src+count + s - src;
-  //         }
-  //       }
-  //     }
-  //   }
-
-  //   if (_min < __min) __min = _min;
-  // }
-
-  console.log(seeds);
-  let seed = [seeds];
-  let result: number[][] = [];
-
+  const __seeds = seeds.reduce(
+    (a, b) => [...a, [b[0], b[0] + b[1] - 1]],
+    [] as number[][]
+  );
   let min = Infinity;
-  for (let s of seeds) {
-    let data: number[][] = [[s[0], s[0] + s[1] - 1]];
-
+  for (let _s of __seeds) {
+    let d = [_s];
     for (let mapType of mapTypes) {
-      let tempCheckRange: number[][] = [];
-      let matchedRange: number[][] = [];
-      console.log("\n??? NEXT SECTION");
-      for (let map of mapType) {
-        let skippedRange: number[][] = [];
-        const [dest, src, count] = map;
-        const mapMin = src;
-        const mapMax = src + count - 1;
-        console.log("\n");
-        console.log("/// NEXT MAP:");
-        console.log("To flow", data);
-        for (let b of data) {
-          const [min, max] = b;
-
-          console.log("\n");
-          console.log("map range", [mapMin, mapMax]);
-          if (mapMax < min || mapMin > max) {
-            skippedRange.push([min, max]);
-            console.log(`Skipped  `, b);
-            continue;
-          }
-          console.log(`Process  `, b);
-
-          const absMin = Math.max(min, mapMin);
-          const absMax = Math.min(max, mapMax);
-
-          console.log("seed     ", [min, max]);
-          console.log("abs      ", [absMin, absMax]);
-
-          let comment = "";
-          // [    x        ]
-          //      [        ]
-          // [....]
-          if (absMin > min) {
-            comment = `${min}___${absMin - 1}` + comment;
-            tempCheckRange.push([min, absMin - 1]);
-          }
-
-          // [        x    ]
-          // [        ]
-          //          [....]
-          if (absMax < max) {
-            comment += `${absMax + 1}___${max}`;
-            tempCheckRange.push([absMax + 1, max]);
-          }
-
-          matchedRange.push([absMin, absMax].map((d) => d - mapMin + dest));
-
-          console.log("Split   >", tempCheckRange, [absMin, absMax]);
-          console.log("Map     >", tempCheckRange, matchedRange);
-          // console.log(`> ${}`)
-          // return tempCheckRange;
-        }
-        if (tempCheckRange.length) {
-          data = tempCheckRange;
-        }
-      }
-      if (matchedRange.length) {
-        data = tempCheckRange.concat(matchedRange);
-      }
+      useLog && console.log(">> Inputs  ", d);
+      useLog && console.log(">> Map type", mapType);
+      d = splitArray(d, mapType);
+      useLog && console.log(">> Result  ", d);
+      useLog && console.log("\n");
     }
-    const min_ = data.reduce(
+
+    const _min = d.reduce(
       (a, b) =>
         Math.min(
           a,
-          b.reduce((c, d) => Math.min(c, d)),
-          Infinity
+          b.reduce((c, d) => Math.min(c, d), Infinity)
         ),
       Infinity
     );
-    console.log({ min_ });
-
-    if (min > min_) {
-      min = min_;
+    useLog && console.log({ _min, min });
+    if (_min < min) {
+      min = _min;
     }
   }
-
-  return min;
-  // for (let i = 0; i < seeds.length / 2; i++) {
-  //   let start = seeds[2 * i];
-  //   const range = seeds[2 * i + 1];
-  //   console.log(start, range);
-
-  //   const [min, max] = [start, start + range - 1];
-
-  //   for (let m of _maps) {
-  //     if (!m) {
-  //       continue;
-  //     }
-  //     if (m.includes(" map:")) {
-  //       // checkpoint
-  //       continue;
-  //     }
-  //   }
-  // }
-
-  // // let result: number[][] = [];
-
-  // let resultMin = Infinity;
-  // let checkRange: number[][] = [];
-
-  // let tempCheckRange: number[][] = checkRange;
-
-  // for (let i = 0; i < seeds.length / 2; i++) {
-  //   {
-  //     let start = seeds[2 * i];
-  //     const range = seeds[2 * i + 1];
-  //     console.log(start, range);
-
-  //     const [min, max] = [start, start + range - 1];
-  //     checkRange = [[min, max]];
-  //     tempCheckRange = checkRange;
-  //     for (let m of _maps) {
-  //       // console.log(m);
-  //       if (!m) {
-  //         if (!tempCheckRange[0]) {
-  //           tempCheckRange = checkRange;
-  //         }
-
-  //         continue;
-  //       }
-  //       if (m.includes(" map:")) {
-  //         console.log(`\n************${m}`);
-  //         checkRange = tempCheckRange;
-  //         tempCheckRange = [];
-  //         // checkpoint
-  //         continue;
-  //       }
-
-  //       console.log("\n\n");
-
-  //       const checkRangeLength = checkRange.reduce(
-  //         (a, b) => a + Math.abs(b[0] - b[1]) + 1,
-  //         0
-  //       );
-
-  //       if (checkRangeLength !== range) {
-  //         console.warn("!!! Not balanced");
-  //         console.log({
-  //           checkRangeLength,
-  //           checkRange,
-  //           tempCheckRange,
-  //         });
-  //         console.warn("!!! Not balanced");
-  //       }
-
-  //       const [dest, src, count] = m.split(" ").map(Number);
-
-  //       const mapMin = src;
-  //       const mapMax = src + count - 1;
-  //       console.log(checkRange);
-  //       console.log(tempCheckRange);
-  //       // console.log("\nCHECK");
-  //       checkRange.forEach((b) => {
-  //         const [min, max] = b;
-  //         if (max < mapMin || min > mapMax) {
-  //           console.log(
-  //             `Skip: ${JSON.stringify({ max, mapMin, min, mapMax })}`
-  //           );
-  //           return;
-  //         }
-
-  //         const absMin = Math.max(min, mapMin);
-  //         const absMax = Math.min(max, mapMax);
-  //         console.log(
-  //           `[${mapMin}...${mapMax}] -> [${dest}...${dest + count - 1}]`
-  //         );
-  //         let comment = `|${absMin}...${absMax}|`;
-
-  //         // [    x        ]
-  //         //      [        ]
-  //         // [....]
-  //         if (absMin > min) {
-  //           comment = `${min}___${absMin - 1}` + comment;
-  //           tempCheckRange.push([min, absMin - 1]);
-  //         }
-
-  //         // [        x    ]
-  //         // [        ]
-  //         //          [....]
-  //         if (absMax < max) {
-  //           comment += `${absMax + 1}___${max}`;
-  //           tempCheckRange.push([absMax + 1, max]);
-  //         }
-  //         console.log(comment);
-  //         tempCheckRange.push([absMin, absMax].map((d) => d - mapMin + dest));
-  //         console.log(tempCheckRange);
-  //         console.log({ min, max, absMin, absMax, mapMin, mapMax });
-  //       });
-
-  //       result = tempCheckRange;
-  //     }
-
-  //     console.log({ checkRange, tempCheckRange });
-  //     const _min = checkRange?.reduce(
-  //       (a, b) =>
-  //         Math.min(
-  //           a,
-  //           b.reduce((c, d) => Math.min(c, d), Infinity)
-  //         ),
-  //       Infinity
-  //     );
-  //     console.log(_min);
-  //     if (resultMin > _min) {
-  //       resultMin = _min;
-  //     }
-  //   }
-  // }
-
-  // return resultMin;
+  useLog && console.log(min);
 }
 main().then(console.log);
-
-// [X] 33942462
-// [X] 33942462
